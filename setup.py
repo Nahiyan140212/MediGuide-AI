5f9a836817dd6d3d9187fc6961e193d263463fae
Nahiyan140212 <nbnoor@ualr.edu>
2025-03-09 13:10:21 -0500

    Folders created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Mediguide Project',
+    version= '0.0.0',
+    author= 'Nahiyan Noor',
+    author_email= 'Nahiyan.cuet@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
